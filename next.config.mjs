7864ec22d2775c6761b40b3cdb0db697440e80be
Phat Luu <yadsendew@protonmail.com>
2024-10-06 20:38:15 +0700

    allow cors

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  crossOrigin: 'anonymous',
   reactStrictMode: true,
   transpilePackages: ["wallet-adapter-react", "wallet-adapter-plugin"],
   assetPrefix: "",
