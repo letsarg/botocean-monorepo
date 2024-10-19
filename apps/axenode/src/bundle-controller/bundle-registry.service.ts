9d76c9f074fe33e798904c55dd1db81865871cdf
Phat Luu <yadsendew@protonmail.com>
2024-10-19 20:02:16 +0700

    finish bundle pull

--- apps/axenode/src/bundle-controller/bundle-registry.service.ts
+++ apps/axenode/src/bundle-controller/bundle-registry.service.ts
@@ -1,14 +1,14 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import { CID } from 'multiformats/cid'
 import fs from 'fs';
 import path from 'path';
-import { create } from 'kubo-rpc-client';
+import { CID, create, IPFSHTTPClient } from 'ipfs-http-client';
 
 @Injectable()
 export class BundleRegistryService implements OnModuleInit {
   private logger = new Logger(BundleRegistryService.name);
-  private helia;
+  private ipfs: IPFSHTTPClient;
   constructor() {
+    this.ipfs = create();
   }
 
   async onModuleInit() {
@@ -17,22 +17,32 @@ export class BundleRegistryService implements OnModuleInit {
 
   async pull(cid: string) {
     try {
-      // const ufs = unixfs(this.helia);
-      const ipfs = create();
-
-      // for await (const file of ufs.cat(CID.parse(cid))) {
-      //   const outputPath = path.join(__dirname, cid);
-      //   fs.appendFileSync(outputPath, file);
-      // }
-
-      for await (const file of ipfs.cat(cid)) {
-        const outputPath = path.join(__dirname, cid);
-        fs.appendFileSync(outputPath, file);
-      }
+      const dirPath = path.join(__dirname, cid);
+      await this.saveDir(CID.parse(cid), dirPath)
 
       this.logger.log(`Pulled bundle CID: ${cid}`);
     } catch (error) {
       this.logger.error(`Error pulling bundle CID: ${cid} err=${error}`);
+    }
+  }
+
+  async saveDir(cid: CID, dirPath: string) {
+    fs.mkdirSync(dirPath, { recursive: true });
+
+    for await (const file of this.ipfs.ls(cid)) {
+      const filePath = path.join(dirPath, file.name)
+      if (file.type === 'file') {
+        await this.saveFile(file.cid, filePath)
+      } else {
+        const childDirPath = path.join(dirPath, file.name)
+        await this.saveDir(cid, childDirPath)
+      }
+    }
+  }
+
+  async saveFile(cid: CID, path: string) {
+    for await (const file of this.ipfs.cat(cid)) {
+      fs.appendFileSync(path, file);
     }
   }
 }
