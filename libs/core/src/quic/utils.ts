dea0d2b695e92d6d222fa0a9b6a14425c26914a5
yadsendew <yadsendew@protonmail.com>
2024-10-19 14:22:15 +0700

    add utils

--- libs/core/src/quic/utils.ts
+++ libs/core/src/quic/utils.ts
@@ -1,4 +1,4 @@
-import { PromiseDeconstructed } from '@matrixai/quic';
+import { PromiseDeconstructed, QUICStream } from '@matrixai/quic';
 import * as peculiarWebcrypto from '@peculiar/webcrypto';
 const webcrypto = new peculiarWebcrypto.Crypto();
 
@@ -231,4 +231,83 @@ export function promise<T = void>(): PromiseDeconstructed<T> {
     resolveP,
     rejectP,
   };
+}
+
+export async function writeTo<T>(stream: QUICStream, msg: T) {
+  const writer = stream.writable.getWriter();
+  const encoder = new TextEncoder();
+  const encReq = encoder.encode(JSON.stringify(msg));
+  let writtenLen = 0;
+  let totalWriteLen = encReq.length;
+  // write the size of msg
+  await writer.write(numberToU8Array(totalWriteLen))
+  console.log(`write len ${numberToU8Array(totalWriteLen)}`)
+  while (writtenLen < totalWriteLen) {
+    let bz = encReq.subarray(writtenLen, writtenLen + 1000)
+    await writer.write(bz)
+    console.log(`write ${bz}`)
+    writtenLen += 1000;
+  }
+}
+
+export async function readFrom<T>(stream: QUICStream): Promise<T> {
+  let collectedEncRes: Uint8Array[] = [];
+  let totalReadLen: number = undefined;
+  let readLen = 0;
+  for await (const encRes of stream.readable) {
+    if (!totalReadLen) {
+      totalReadLen = u8ArrayToNumber(encRes);
+    } else {
+      collectedEncRes.push(encRes)
+      readLen += encRes.length
+      if (readLen == totalReadLen) {
+        break;
+      }
+    }
+  }
+
+  let encRes = combineUint8Arrays(collectedEncRes);
+  const decoder = new TextDecoder('utf-8');
+  let jsonRes = decoder.decode(encRes)
+  let res: T = JSON.parse(jsonRes);
+  return res
+}
+
+function combineUint8Arrays(arrays: Uint8Array[]): Uint8Array {
+  // Calculate the total length
+  const totalLength = arrays.reduce((sum, arr) => sum + arr.length, 0);
+
+  // Create a new Uint8Array with the total length
+  const combinedArray = new Uint8Array(totalLength);
+
+  // Keep track of the current index
+  let offset = 0;
+
+  // Copy each array into the combined array
+  arrays.forEach(arr => {
+    combinedArray.set(arr, offset);
+    offset += arr.length;
+  });
+
+  return combinedArray;
+}
+
+function numberToU8Array(num): Uint8Array {
+  if (num < 0 || num > 65535) {
+    throw new RangeError('Number must be between 0 and 65535');
+  }
+  const highByte = (num >> 8) & 0xFF;
+  const lowByte = num & 0xFF;
+  return new Uint8Array([highByte, lowByte]);
+}
+
+function u8ArrayToNumber(u8Array): number {
+  if (u8Array.length !== 2) {
+    throw new Error('Uint8Array must have exactly 2 elements');
+  }
+
+  const highByte = u8Array[0];  // Most significant byte
+  const lowByte = u8Array[1];   // Least significant byte
+
+  return (highByte << 8) | lowByte;
 }
